f0dd8956c03eaa00a014c9d22e4edce5b65473b1
watanas23-meet <pintelzyt@gmail.com>
2022-08-01 13:49:38 +0300

    added-bonus

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,5 @@
-def  create_youtube_video(title, descreption):
-    dict_vid = {"title": title, "descreption": descreption, "likes" : 0, "dislikes" : 0, "conmments" : {"username" : ""}, }
+def  create_youtube_video(title, descreption,hashtag):
+    dict_vid = {"title": title, "descreption": descreption, "likes" : 0, "dislikes" : 0, "conmments" : {"username" : ""}, "hashtag": hashtag }
     return dict_vid 
 
 def likes(vid):
@@ -19,4 +19,24 @@ def add_comment(youtubevideo, username, comment_text):
 
     return youtubevideo
 
-video = create_youtube_video("NEW HACK FOR ROBLOX!!!! | FREE RUBOX 1M!!!!", "CLICK THE LINK BELOW FRO 100M RUBOX!!!!! : https://sdkjf.asdasdasd/freerubox")
+
+
+video = create_youtube_video("NEW HACK FOR ROBLOX!!!! | FREE RUBOX 1M!!!!", "CLICK THE LINK BELOW FRO 100M RUBOX!!!!! : https://sdkjf.asdasdasd/freerubox", {"fun", "roblox", "dada","robux","new"})
+video_2 = create_youtube_video("NEW HACK FOR FORTNITE!!!! | FREE VBUCKS 1M!!!!", "CLICK THE LINK BELOW FRO 100M VBUCKS!!!!! : https://sdkjf.asdasdasd/freeFBUCKS", {"fun", "fortnite", "dada","vbucks","new"})
+
+def similarity_to_video(vid1, vid2):
+    count = 0
+    for i in vid1["hashtag"]:
+        for x in vid2["hashtag"]:
+            if x == i:
+                count = count + 1
+    
+    result = (count / 5) * 100
+
+    return result
+
+
+# add 495 likes
+
+for i in 495:
+    likes(video)
